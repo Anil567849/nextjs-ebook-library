ded029ac5d708775e2db3e2b17f16852a7b20a7b
Anil Kumar Behera <anilkumar567849@gmail.com>
2024-08-25 15:18:37 +0530

    book card created

---
+++ src/app/(home)/components/BookList.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import BookCard from './BookCard';
+import { Book } from '@/types';
+
+async function BookList(){
+    // data fetching
+    // const books = await fetchBooks();
+
+    const books = await [
+                {
+                  _id: 'anil',
+                  title: "Think and Grow Rich",
+                  description: "Amazing Book. Keep learning and keep growing",
+                  coverImage: "https://images.unsplash.com/photo-1532012197267-da84d127e765?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  file: "file",
+                  author: {
+                    name: "Anil Kumar and N. Hill"
+                  },
+                },
+                {
+                  _id: 'anil1',
+                  title: "Think and Grow Rich",
+                  description: "Amazing Book. Keep learning and keep growing",
+                  coverImage: "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  file: "file",
+                  author: {
+                    name: "Anil Kumar and N. Hill"
+                  },
+                },
+                {
+                  _id: 'anil2',
+                  title: "Think and Grow Rich",
+                  description: "Amazing Book. Keep learning and keep growing",
+                  coverImage: "https://images.unsplash.com/photo-1535905557558-afc4877a26fc?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  file: "file",
+                  author: {
+                    name: "Anil Kumar and N. Hill"
+                  },
+                }
+            ]
+
+    return (
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
+            {books.map((book: Book) => (
+                <BookCard key={book._id} book={book} />
+            ))}
+        </div>
+    );
+};
+
+async function fetchBooks(){
+  const response = await fetch(`${process.env.BACKEND_URL}/books`, { cache: 'no-store' });
+  if (!response.ok) {
+      throw new Error('An error occurred while fetching the books');
+  }
+
+  return await response.json();
+}
+
+export default BookList;
+
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,10 +2,19 @@
 
 // (home) = next js will not consider it as route - we use it if we want to group components of page. Example: if you have x components of home page which you will use only in home page then you can put all its component inside (home)
 
-import Image from "next/image";
+// by default next.js pages.tsx are server components if you want to convert it to client component then use 'use client' at the top. 
+// you can also make server Component async so that you can fetch data from api 
+
+import { Suspense } from "react";
+import BookList from "./components/BookList";
+import Loading from "@/components/Loading";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+      <>
+          <Suspense fallback={<Loading />}>
+              <BookList />
+          </Suspense>
+      </>
   );
 }
